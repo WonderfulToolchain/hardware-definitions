6b245fe57f7838a3c7fdf83f701dc1b16087b723
Adrian Siekierka <kontakt@asie.pl>
2025-05-10 16:54:38 +0200

    target/fc: initial ports

--- build.sh
+++ build.sh
@@ -4,8 +4,12 @@ if [ -d dist ]; then
     rm -r dist
 fi
 
+mkdir -p dist/c/fc
+python generate.py -f c --define-guard FC_MMIO_H_ -o dist/c/fc/mmio.h target/fc
+
 mkdir -p dist/c/wswan
 python generate.py -f c --define-guard WS_PORTS_H_ -o dist/c/wswan/ports.h target/wswan
 
 mkdir -p dist/nasm/wswan
 python generate.py -f nasm -o dist/nasm/wswan/ports.inc target/wswan
+

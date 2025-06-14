021d7d92c2401c22692237506a4aeba053743625
Adrian Siekierka <kontakt@asie.pl>
2025-06-14 15:19:50 +0200

    target/gba: initial ports

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ fi
 mkdir -p dist/c/fc
 python generate.py -f c --define-guard FC_MMIO_H_ -o dist/c/fc/mmio.h target/fc
 
+mkdir -p dist/c/gba
+python generate.py -f c --define-guard GBA_MMIO_H_ -o dist/c/gba/mmio.h target/gba
+
 mkdir -p dist/c/wswan
 python generate.py -f c --define-guard WS_PORTS_H_ -o dist/c/wswan/ports.h target/wswan
 
